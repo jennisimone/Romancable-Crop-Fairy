5616bed09521e9c093564f5637c0ecaa2d088211
jennisimone <jennisimone@gmail.com>
2026-03-02 22:43:11 +0000

    Finish content, with some bugs.

---
+++ assets/maps/flowers.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="flowers" tilewidth="16" tileheight="32" tilecount="16" columns="2">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="flowers.png" width="32" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="2.79939" y="27.377" width="10.8257" height="4.54043"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.27632" y="10.1316" width="9.01316" height="4.27632"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.01316" y="11.5132" width="7.89474" height="3.88158"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.84211" y="12.8289" width="3.22368" height="2.43421"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.26316" y="11.7763" width="7.03947" height="2.96053"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.53947" y="11.7763" width="8.22368" height="2.76316"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.73684" y="11.5789" width="8.15789" height="3.42105"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Passable" value="T"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.94737" y="12.2368" width="8.61842" height="3.68421"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/maps/frogs.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="frogs" tilewidth="16" tileheight="16" tilecount="18" columns="3">
+ <image source="frogs.png" width="48" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.73684" y="12.8947" width="7.23684" height="2.76316"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="180"/>
+   <frame tileid="0" duration="180"/>
+   <frame tileid="2" duration="180"/>
+   <frame tileid="1" duration="180"/>
+   <frame tileid="0" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.21053" y="12.1711" width="8.28947" height="3.81579"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="3" duration="180"/>
+   <frame tileid="3" duration="180"/>
+   <frame tileid="3" duration="180"/>
+   <frame tileid="5" duration="180"/>
+   <frame tileid="4" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.21053" y="11.9079" width="8.22368" height="3.75"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="6" duration="180"/>
+   <frame tileid="8" duration="180"/>
+   <frame tileid="7" duration="180"/>
+   <frame tileid="6" duration="180"/>
+   <frame tileid="6" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.40789" y="12.1711" width="6.97368" height="3.55263"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="9" duration="180"/>
+   <frame tileid="11" duration="180"/>
+   <frame tileid="10" duration="180"/>
+   <frame tileid="9" duration="180"/>
+   <frame tileid="9" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="5">
+   <object id="5" x="3.75" y="11.9737" width="6.97368" height="3.55263"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="12" duration="180"/>
+   <frame tileid="14" duration="180"/>
+   <frame tileid="13" duration="180"/>
+   <frame tileid="12" duration="180"/>
+   <frame tileid="12" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="5">
+   <object id="4" x="4.53948" y="12.5658" width="6.97368" height="3.55263"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="15" duration="180"/>
+   <frame tileid="17" duration="180"/>
+   <frame tileid="16" duration="180"/>
+   <frame tileid="15" duration="180"/>
+   <frame tileid="15" duration="180"/>
+  </animation>
+ </tile>
+</tileset>
